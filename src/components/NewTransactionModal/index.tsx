e5793c94bbfd0c6ba9c0c99340e20632f302adae
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-13 13:18:50 -0300

    form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from './styles';
 import Modal from "react-modal";
+import closeImg from "../../assets/close.svg";
 
 interface NewTransactionModalProps{
     isOpen: boolean;
@@ -9,12 +10,17 @@ interface NewTransactionModalProps{
 export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionModalProps){
     return (
         <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
+            <button type="button">
+                <img src={closeImg} alt="Fechar" onClick={onRequestClose} className="react-modal-close" />
+            </button>
             <Container>
                 <h2>Cadastrar transacao</h2>
 
                 <input type="text" placeholder='Titulo' />
                 <input type="number" placeholder='Valor' />
                 <input type="text" placeholder='Categoria' />
+
+                <button type="submit">Cadastrar</button>
             </Container>
         </Modal>
     )
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -56,4 +56,17 @@ export const GlobalStyle = createGlobalStyle`
         position: relative;
         border-radius: 0.25r4em;
     }
+
+    .react-modal-close{
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+        transition: filter 0.2s;
+
+        &:hover{
+            filter: brightness(0.8);
+        }
+    }
 `;
